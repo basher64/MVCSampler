6e8d8367845a0247069adad1f4ff418c674217df
Bill Stewart <bstewart@vegas.local>
2018-11-25 15:53:21 -0700

    Initial Commit

---
+++ MVCSampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCSampler
+//
+//  Created by Bill Stewart on 11/25/18.
+//  Copyright © 2018 Bill Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
